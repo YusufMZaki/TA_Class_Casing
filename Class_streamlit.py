fa27b72fd444e8650b69bb094387f590f1f842f6
YusufMZaki <148864190+YusufMZaki@users.noreply.github.com>
2024-06-16 17:19:32 +0000

    Organice

--- Class_streamlit.py
+++ Class_streamlit.py
@@ -9,15 +9,9 @@ import numpy as np
 import altair as alt 
 st.set_page_config(layout="wide")
 
-with st.form("my_form"):
-   st.write("Inside the form")
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
-
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
+form = st.form("my_form")
+form.slider("Inside the form")
+form.form_submit_button("Submit")
 
 # Data Mentah Properties Casing Design
 Casing_data_ = pd.read_excel(io='Casing_Simplify.xlsx', skiprows=[0, 1], header=[0]) 
